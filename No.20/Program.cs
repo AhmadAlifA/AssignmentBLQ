a1ab0da644fcde5392d9d49b06dc0b416ee06365
AhmadAlifA <125856322+AhmadAlifA@users.noreply.github.com>
2023-12-21 23:02:24 +0700

    Perbaikan No.20

--- No.20/Program.cs
+++ No.20/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace No._20
 {
@@ -10,12 +11,13 @@ namespace No._20
             Console.WriteLine("Permainan Suit Gunting Batu Kertas");
 
             int jarakAwal = 2;
-            string suitA = "GGG", suitB = "KKB";
+            //string suitA = "GGG", suitB = "KKB";
+            string suitA = "KKB", suitB = "GGG";
 
             Dictionary<string, int> aturanSuit = new Dictionary<string, int>
             {
-            {"GB", 2}, {"BK", 2}, {"KG", 2},
-            {"BG", -1}, {"KB", -1}, {"GK", -1}
+            {"GB", -1}, {"BK", -1}, {"KG", -1},
+            {"BG", 2}, {"KB", 2}, {"GK", 2}
             };
 
             string pemenang = TentukanPemenang(jarakAwal, suitA, suitB, aturanSuit);
@@ -24,18 +26,22 @@ namespace No._20
         }
         static string TentukanPemenang(int jarakAwal, string suitA, string suitB, Dictionary<string, int> aturanSuit)
         {
-            int jarakAntaraAB = jarakAwal;
+            int posisiA = (int) Math.Ceiling((double)jarakAwal / 2);
+            int posisiB = posisiA - jarakAwal; 
             for (int i = 0; i < suitA.Length; i++)
             {
-                string kombinasiSuit = $"{suitA[i]}{suitB[i]}";
-                if (aturanSuit.ContainsKey(kombinasiSuit))
+                if (suitA[i] != suitB[i])
                 {
-                    jarakAntaraAB += aturanSuit[kombinasiSuit];
-                    Console.WriteLine(jarakAntaraAB);
-                }
-                if (jarakAntaraAB == 0)
-                {
-                    return "A";
+                    string kombinasiSuit = $"{suitA[i]}{suitB[i]}";
+                    string kombinasiSuit1 = $"{suitB[i]}{suitA[i]}";
+
+                    posisiA -= aturanSuit[kombinasiSuit];
+                    posisiB += aturanSuit[kombinasiSuit1];
+                        Console.WriteLine(posisiA + " || " +posisiB);
+                    if (posisiA < posisiB || posisiA - posisiB == 0 )
+                    {
+                        return aturanSuit[kombinasiSuit] == 2 ? "A" : "B"; 
+                    }
                 }
             }
             return "Draw";
